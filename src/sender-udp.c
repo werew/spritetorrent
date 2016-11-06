9e3bacf433891bce9ce930842637a9041c2f92b3
Victor <victor.constans@etu.unistra.fr>
2016-11-07 00:31:59 +0100

    Begin code client programm

---
+++ src/sender-udp.c
@@ -0,0 +1,143 @@
+/**
+ * @file sender-udp.c
+ * @author Julien Montavont
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @section DESCRIPTION
+ *
+ * Simple program that creates an IPv4 UDP socket and sends a string
+ * to a remote host. The string, IPv4 addr and port number of the
+ * remote host are passed as command line parameters as follow:
+ * ./pg_name IPv4_addr port_number string
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+# include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "../include/types.h"
+
+int main(int argc, char **argv)
+{
+	int sockfd_track,sockfd_peer;
+	socklen_t addrlen;
+	struct sockaddr_in tracker, peer;
+
+	// check the number of args on command line
+	if(argc != 6)
+	{
+		printf("USAGE: %s @tracker tracker_port listen_port mode hash\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	// socket factory
+	if((sockfd_track = socket(AF_INET,SOCK_DGRAM,0)) == -1)
+	{
+		perror("Erreur socket tracker");
+		exit(EXIT_FAILURE);
+	}
+
+	// init remote addr structure and other params
+	tracker.sin_family = AF_INET;
+	tracker.sin_port   = htons(atoi(argv[2]));
+	addrlen         = sizeof(struct sockaddr_in);
+
+	// get addr from command line and convert it
+	if(inet_pton(AF_INET,argv[1],&tracker.sin_addr.s_addr) != 1)
+	{
+		perror("Erreur inet_pton");
+		close(sockfd_track);
+		exit(EXIT_FAILURE);
+	}
+
+/******************************************************************/
+
+	if((sockfd_peer = socket(AF_INET,SOCK_DGRAM,0)) == -1)
+	{
+		perror("Erreur socket peer");
+		close(sockfd_track);
+		exit(EXIT_FAILURE);
+	}
+
+	// init remote addr structure and other params
+	peer.sin_family = AF_INET;
+	peer.sin_port   = htons(atoi(argv[3]));
+	peer.sin_addr.s_addr=INADDR_ANY;
+
+	if(bind(sockfd_peer,(struct sockaddr *)&peer,addrlen)==-1)
+	{
+		perror("Erreur bind");
+		close(sockfd_track);
+		close(sockfd_peer);
+		exit(EXIT_FAILURE);
+	}
+
+/***********************************************************************/
+
+	struct msg query, answer;
+
+	if(strcmp(argv[4],"put")==0)
+	{
+		query.type=110;
+	}
+	else if(strcmp(argv[4],"get")==0)
+	{
+		query.type=112;
+	}
+	else
+	{
+		printf("Erreur mode");
+		close(sockfd_track);
+		exit(EXIT_FAILURE);
+	}
+
+	query.length=strlen(argv[5]);
+
+	query.data=malloc(query.length);
+
+	memcpy(query.data,argv[5],query.length);
+
+	// send query to tracker
+	if(sendto(sockfd_track,(void*)&query,sizeof(struct msg),0,(const struct sockaddr *) &tracker,addrlen) == -1)
+	{
+		perror("Erreur send");
+		close(sockfd_track);
+		exit(EXIT_FAILURE);
+	}
+
+	answer.data=malloc(1024);
+
+	if(recvfrom(sockfd_track,&answer,1027,0,NULL,NULL) == -1)	//TODO mettre info tracker dans sockaddr
+   	{
+		perror("Erreur recvfrom");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+
+	// close the socket
+	close(sockfd_track);
+	close(sockfd_peer);
+
+	return 0;
+}
