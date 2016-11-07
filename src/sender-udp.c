74d654ec079dcd62d78b5af177ce5394dd0d135f
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-07 12:34:29 +0100

    Removing J.M. stuffs

--- src/sender-udp.c
+++ src/sender-udp.c
@@ -1,32 +1,6 @@
-/**
- * @file sender-udp.c
- * @author Julien Montavont
- * @version 1.0
- *
- * @section LICENSE
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @section DESCRIPTION
- *
- * Simple program that creates an IPv4 UDP socket and sends a string
- * to a remote host. The string, IPv4 addr and port number of the
- * remote host are passed as command line parameters as follow:
- * ./pg_name IPv4_addr port_number string
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-# include <sys/types.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <inttypes.h>
