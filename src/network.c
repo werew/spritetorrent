cac9d4a76a281d2377daa1d7cdfbd2ae36abe221
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-11 17:32:44 +0100

    Add includes

--- src/network.c
+++ src/network.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "../include/types.h"
+
+
 /**
  * Creates a socket and binds it to the given address
  * and port.
