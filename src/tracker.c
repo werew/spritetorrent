ea8d88e3df8fe7b2f5ee733825fdace305202930
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-11 18:17:56 +0100

    Tracker can receive a msg

--- src/tracker.c
+++ src/tracker.c
@@ -12,10 +12,21 @@
 #include <string.h>
 #include "../include/types.h"
 
-
-
+// Max size of a msg
+#define MAX_MSG 1028
 
 int main(int argc, char* argv[]){
+    // Listen on localhost port 5555
+    // XXX those values are just a test
+    int socketfd = init_connection("localhost",5555);
 
+    char buf[MAX_MSG];
+    struct sockaddr_in src_addr;
+    socklen_t addrlen = sizeof addrlen;
+   
+    // Receive a msg  
+    recvfrom(socketfd, buf, MAX_MSG, 0, 
+             (struct sockaddr*) &src_addr, &addrlen);
+    
     return 0;
 }
