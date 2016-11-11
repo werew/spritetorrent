d6ac12b8c9e5eb4b790d018a0c460b33868f2303
VConstans <victor.constans@etu.unistra.fr>
2016-11-11 18:02:58 +0100

    Socket configuration by bind in init_connection function

--- src/network.c
+++ src/network.c
@@ -1,24 +1,26 @@
 
 
 
-void init_connection(char* addr, uint16_t port){	
+void init_connection(char* addr, uint16_t port){
 
 	int sockfd;
-	socklen_t addrlen;
 	struct sockaddr_in sockaddr;
 
 	/* TODO  AF_INET changes depending on the type
  	   of address (ipv4 or ipv6) */
 
+	//Création socket
 	if((sockfd = socket(AF_INET,SOCK_DGRAM,0)) == -1){
 		perror("Erreur socket tracker");
+		//TODO traitement de l'erreur
 	}
 
 	sockaddr.sin_family = AF_INET;
 	sockaddr.sin_port   = htons(port);
+	sockaddr.sin_addr.s_addr = INADDR_ANY;
 
-	if(inet_pton(AF_INET,argv[1],&sockaddr.sin_addr.s_addr) != 1){
-		perror("Erreur inet_pton");
-		close(sockfd_track);
+	//Configuration du socket
+	if(bind(sockfd,(struct sockaddr *)&sockaddr,sizeof(struct sockaddr_in)) == -1){
+		//TODO traitement de l'erreur
 	}
 }
