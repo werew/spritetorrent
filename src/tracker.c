90d9dfd2b90eb1d1ef1cf65d7699a0341e50f1bd
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-11 18:35:15 +0100

    Function accept message

--- src/tracker.c
+++ src/tracker.c
@@ -15,18 +15,35 @@
 // Max size of a msg
 #define MAX_MSG 1028
 
+
+/**
+ * Accept a message and deals with it
+ * @param sockfd A socket file descriptor
+ *        from where receive the message
+ * @return 0 in case of success, -1 otherwise
+ */
+int accept_msg(int sockfd){
+
+    struct msg* msg = malloc(MAX_MSG);
+    struct sockaddr_in src_addr;
+    socklen_t addrlen;
+   
+    // Receive a msg  
+    // TODO test for errors
+    recvfrom(sockfd, msg, MAX_MSG, 0, 
+             (struct sockaddr*) &src_addr, &addrlen);
+
+    // TODO deal with the content of the msg
+
+}
+
 int main(int argc, char* argv[]){
     // Listen on localhost port 5555
     // XXX those values are just a test
-    int socketfd = init_connection("localhost",5555);
+    int sockfd = init_connection("127.0.0.1",5555);
 
-    char buf[MAX_MSG];
-    struct sockaddr_in src_addr;
-    socklen_t addrlen = sizeof addrlen;
-   
-    // Receive a msg  
-    recvfrom(socketfd, buf, MAX_MSG, 0, 
-             (struct sockaddr*) &src_addr, &addrlen);
+    accept_msg(sockfd);
+
     
     return 0;
 }
