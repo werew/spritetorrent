3b63e211680289a0e7770fa2fa3792db17a7d29b
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2016-11-12 02:18:26 +0100

    New prototype for handle_msg

--- src/tracker.c
+++ src/tracker.c
@@ -23,26 +23,23 @@
  *        pointed by src_addr
  * @return 0 in case of success, -1 otherwise
  */
-int handle_msg
-(struct msg* msg, struct sockaddr* src_addr, socklen_t addrlen){
-/*
+int handle_msg(struct acpt_msg* am){
+
     // Check valid length
-    uint16_t mlenght = msgget_length(msg);
-    if (mlenght > size_msg) {
-        drop_msg(msg);
+    uint16_t mlenght = msgget_length(am->msg);
+    if (mlenght > am->size) {
         printf("Message dropped: bad length\n"); 
         return 0;
     }
 
-*/
-    return 0;
     // Create seeder
-    struct seeder* peer = create_seeder(src_addr,addrlen);
+    struct seeder* peer = create_seeder
+            ((struct sockaddr*) &am->src_addr,am->addrlen);
     if (peer == NULL) return -1;
     
 
     //TODO complete
-    switch (msg->type){
+    switch (am->msg->type){
         case PUT_T: puts("PUT");
             break;
         case GET: puts("GET");
@@ -63,14 +60,11 @@ int main(int argc, char* argv[]){
     int sockfd = init_connection("127.0.0.1",5555);
 
     struct acpt_msg* am = accept_msg(sockfd);
-/*
-    // Deal with the msg
-    if (handle_msg(msg, (struct sockaddr*) &src_addr, addrlen) == -1){
-        drop_msg(msg);
-        return -1;
-    }
-*/
 
+    // Deal with the msg
+    if (handle_msg(am) == -1) perror("");
+
+    drop_acpt_msg(am);
     return 0;
 }
-
+  
